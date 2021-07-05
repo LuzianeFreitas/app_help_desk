6b1accd75cc7287cc56867c45ca68173fdf5cfad
Luziane Freitas <freitas.lu@outlook.com>
2021-07-05 10:26:56 -0300

    add: routes

--- valida_login.php
+++ valida_login.php
@@ -19,8 +19,8 @@
     }
 
     if($usuario_autenticado) {
-        echo 'Sucesso ao efetuar login';
         $_SESSION['autenticado'] = 'SIM';
+        header('Location: home.php');
     } else {
         $_SESSION['autenticado'] = 'NAO';
         header('Location: index.php?login=erro');
