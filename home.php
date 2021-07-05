76dcda7f7e170c87b1f6d56c35914dd034479b14
Luziane Freitas <freitas.lu@outlook.com>
2021-07-05 10:17:40 -0300

    refactoring session

--- home.php
+++ home.php
@@ -1,10 +1,5 @@
 <?php
-  session_start();
-
-  if(!isset($_SESSION['autenticado']) || $_SESSION['autenticado'] != 'SIM') {
-    header('Location: index.php?login=erro2');
-  }
-
+  require_once 'validador_acesso.php'
 ?>
 <html>
   <head>
