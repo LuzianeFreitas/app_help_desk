9cf63e23b35fed985f49b283a36ba35d53727907
Luziane Freitas <freitas.lu@outlook.com>
2021-07-05 11:11:20 -0300

    add: registro de chamados

---
+++ registra_chamado.php
@@ -0,0 +1,20 @@
+<?php
+
+    // Montagem do texto
+
+    $titulo = str_replace('#', '-', $_POST['titulo']);
+    $categoria = str_replace('#', '-', $_POST['categoria']);
+    $descricao = str_replace('#', '-', $_POST['descricao']);
+
+    $texto = $titulo .'#'. $categoria .'#'. $descricao . PHP_EOL; 
+
+    // Registrando o chamado em um arquivo
+
+    $arquivo = fopen('arquivo.hd','a');
+
+    fwrite($arquivo,$texto);
+
+    fclose($arquivo);
+
+    header('Location: abrir_chamado.php')
+?>
